442ad6f567db823843ab36f6bab4678a61b91fb3
Vitalii Tron <vittron85@gmail.com>
2020-09-30 01:05:36 +0300

    Developed frontend theme

--- themes/patronat/assets/js/main.js
+++ themes/patronat/assets/js/main.js
@@ -1,1 +1,8 @@
-window.$ = window.jQuery = require('jquery');
+$('#form_question_form button.btn')
+  .parent()
+  .removeClass('col-12')
+  .addClass('col-lg-9')
+  .addClass('col-md-8')
+  .addClass('offset-lg-3')
+  .addClass('offset-md-4')
+  .css('padding', 0);
